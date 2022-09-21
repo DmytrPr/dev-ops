24b4d2545b5bd03c4fe21d228ef383fd424f5e88
Дмитрий Прищепа <dmitr@DESKTOP-7FIL2VE.localdomain>
2022-09-21 19:41:33 +0300

    feat(lab1): rerun command

---
+++ lab1/pryshchepa/tests/dmytro-1-test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env roundup
+#
+#/ usage:  rerun stubbs:test -m pryshchepa -p dmytro [--answers <>]
+#
+
+# Helpers
+# -------
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+describe "dmytro"
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
